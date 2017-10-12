049a3260ba454f8c6eade51e3c781d5f9870900b
emuio <emuio@emuio.local>
2017-10-12 10:11:03 +0800

    use pos-tip to search word

--- lisp/init-youdao-dictionary.el
+++ lisp/init-youdao-dictionary.el
@@ -1,4 +1,4 @@
 (require-package 'youdao-dictionary)
 (setq url-automatic-caching t)
-(global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point)
+(global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point+)
 (provide 'init-youdao-dictionary)
