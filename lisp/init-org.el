1985afbde6489a5aac724b7a14924fe97c7cfe6f
emuio <emuio@emuio.local>
2017-11-06 10:41:58 +0800

    org-cycle-agenda-files is broken in org-mode 8.2.10

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -484,4 +484,23 @@ typical word processor."
 ;;; org-brain
 ;;; (maybe-require-package 'org-brain)
 
+(defun org-cycle-agenda-files ()
+  "Cycle through the files in `org-agenda-files'.
+If the current buffer visits an agenda file, find the next one in the list.
+If the current buffer does not, find the first agenda file."
+  (interactive)
+  (let* ((fs (org-agenda-files t))
+         (files (append fs (list (car fs))))
+         (tcf (if buffer-file-name (file-truename buffer-file-name)))
+         file)
+    (unless files (user-error "No agenda files"))
+    (catch 'exit
+      (while (setq file (pop files))
+        (if (equal (file-truename file) tcf)
+            (when (car files)
+              (find-file (car files))
+              (throw 'exit t))))
+      (find-file (car fs)))
+    (if (buffer-base-buffer) (org-pop-to-buffer-same-window (buffer-base-buffer)))))
+
 (provide 'init-org)
