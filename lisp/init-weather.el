4efc795ed21dcf7b188c99b083d37cfcbeda74e4
emuio <emuio@emuio-2.local>
2018-01-27 18:47:55 +0800

    soft require package wttrin, which required emacs 24.4.

--- lisp/init-weather.el
+++ lisp/init-weather.el
@@ -1,5 +1,4 @@
-(require-package 'wttrin)
-
-(setq wttrin-default-cities '("Shanghai"))
+(when (maybe-require-package 'wttrin)
+  (setq wttrin-default-cities '("Shanghai")))
 
 (provide 'init-weather)
