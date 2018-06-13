681a5133b42af5973d391a22a2b8e0f935e1ed8b
emuio <emuio@hotmail.com>
2018-06-13 10:15:16 +0800

    soft require ox-reveal

--- lisp/init-ppt.el
+++ lisp/init-ppt.el
@@ -1,9 +1,10 @@
 ;;----------------------------------------------------------------------------
 ;; ox-reveal
 ;;----------------------------------------------------------------------------
-;; (require-package 'ox-reveal)
+(when (maybe-require-package 'ox-reveal)
+  (setq org-reveal-root "./reveal.js")
+  (add-hook 'org-mode-hook (lambda () (load-library "ox-reveal"))))
 
-(setq org-reveal-root "./reveal.js")
-(add-hook 'org-mode-hook (lambda () (load-library "ox-reveal")))
+
 
 (provide 'init-ppt)
