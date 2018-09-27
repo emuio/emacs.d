7f1514cdbd2fbee15639120bc3222627853483ca
emuio <emuio@hotmail.com>
2018-09-27 19:24:48 +0800

    add go-mode config

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -2,8 +2,20 @@
 (unless (version< emacs-version "24.3")
   (require-package 'company-go))
 
+;;autocomplete
+(add-hook 'go-mode-hook 'company-mode)
+(add-hook 'go-mode-hook
+          (lambda ()
+            (set (make-local-variable 'company-backends) '(company-go))
+            (company-mode)))
 
 (setenv "GOPATH" "~/go")
 
+;; Call Gofmt before saving
+(setq gofmt-command "goimports")
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+;; Godef jump key binding
+(define-key go-mode-map (kbd "M-.") 'godef-jump)
 
 (provide 'init-go)
