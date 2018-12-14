07e575ddcd7a194cc3555b8ec22fef62a063e699
emuio <emuio@hotmail.com>
2018-12-14 12:14:51 +0800

    fix typo

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -331,7 +331,7 @@ With arg N, insert N newlines."
 
 
 
-(when (and ((version< "25.1" emacs-version))
+(when (and (version< "25.1" emacs-version)
            (require-package 'highlight-indent-guides))
   (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
   (setq highlight-indent-guides-method 'character))
